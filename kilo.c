1d959c3cc6f49c80a633e0dafa0501de9a653966
Abdulkerim Akten <prefront4ll0b@gmail.com>
2026-04-17 23:31:55 +0300

    Initial clean commit

---
+++ kilo.c
@@ -0,0 +1,63 @@
+/*** includes ***/
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
+
+
+/*** data ***/
+
+struct termios original_termios;
+
+
+/*** terminal ***/
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1)
+        die("tcsetattr");
+}
+
+void enableRawMode() {
+    if (tcgetattr(STDIN_FILENO, &original_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode);
+
+    struct termios raw = original_termios;
+
+    raw.c_iflag &= ~(BRKINT | ISTRIP | INPCK | ICRNL | IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+
+/*** init ***/
+
+int main() {
+    enableRawMode();
+
+    while(1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+        if(iscntrl(c)){
+            printf("%d\r\n", c);
+        } 
+        else {
+            printf("%d ('%c')\r\n",c ,c);
+        }
+        if (c == 'q') break;
+    };
+
+    return 0;
+}
